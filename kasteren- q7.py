38ac28d5ea43b58a597f8dbf8e170a539419c390
IsaacTKD <mhtong22@hotmail.com>
2017-02-25 21:15:28 -0500

    Update - kasteren- q7

--- kasteren- q7.py
+++ kasteren- q7.py
@@ -5,13 +5,36 @@ from datetime import datetime
 
 #-----------------------------------------------------------------------------
 #7
+
+#Activities
+plt.style.use('ggplot')
 df = pd.read_csv('KasterenActData.txt', header=None)
 df.drop(df.index[[0,1,2,3,4,5,6,7,8,9,10,11,12]], inplace=True)
 df = pd.DataFrame(df[0].str.split('\t',2).tolist(), columns=['StartTime', 'EndTime', 'ID'])
 df['StartTime'] = pd.to_datetime(df['StartTime'], errors='coerce')
 df['EndTime'] = pd.to_datetime(df['EndTime'], errors='coerce')
 df = df.drop(df.index[245])
-print df
+
+df['ID'] = pd.to_numeric(df['ID'], errors='coerce')
+plt.hist(df['ID'], bins =30)
+plt.show()
+
+#Sensors
+df = pd.read_csv('KasterenSenseData.txt', header=None)
+df.drop(df.index[[0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]], inplace=True)
+df = pd.DataFrame(df[0].str.split('\t',3).tolist(), columns=['StartTime', 'EndTime', 'ID', ' Val'])
+df['StartTime'] = pd.to_datetime(df['StartTime'], errors='coerce')
+df['EndTime'] = pd.to_datetime(df['EndTime'], errors='coerce')
+df = df.drop(df.index[1319])
+
+df['ID'] = pd.to_numeric(df['ID'], errors='coerce')
+plt.hist(df['ID'], bins = 30)
+plt.show()
+
+# Result 
+# From the activity data, preparing dinner is rarely done by the subject
+# From the sensor data, subject rarely washes dishes 
+
 
 
 
