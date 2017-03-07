f196555daee58f51cfeaa8f9f03edf88d343b159
IsaacTKD <mhtong22@hotmail.com>
2017-03-07 01:45:48 -0500

    Update Kmean

--- 2. ARAS datasets/kmean.py
+++ 2. ARAS datasets/kmean.py
@@ -7,36 +7,85 @@ from sklearn.cluster import KMeans
 plt.style.use('ggplot')
 
 #-----------------------------------------------------------------------------
-# from 9 - 10
-columnsName = ['Ph1','Ph2','Ir1','Fo1','Fo2','Di3','Di4','Ph3','Ph4','Ph5','Ph6','Co1','Co2','Co3','So1','So2','Di1','Di2','Te1','Fo3','R1','R2']
-fig = plt.figure()
-ax = fig.add_subplot(111)
+#columnsName = ['Ph1','Ph2','Ir1','Fo1','Fo2','Di3','Di4','Ph3','Ph4','Ph5','Ph6','Co1','Co2','Co3','So1','So2','Di1','Di2','Te1','Fo3','R1','R2']
+#fig = plt.figure(figsize=(10,7))
+#ax = fig.add_subplot(111)
+#
+## 1
+## Number of occurrence per Day
+## Data : 30 days of ARAS data
+#
+#df = pd.read_csv('House A/ARAS_Datasets.txt', sep=" ", header=None)
+#df.columns = columnsName
+## select only 10 days because it takes too long to load 30 days of data
+#df = df.loc[df.index < 864001]
+#activitiesPerDay = []
+#activitiesPerDay.append(df.R1[0])
+#unique = []
+#unique = df.R1.unique()
+#d = {}
+#for i in range (0, len(df)-1):
+#    if(df.R1[i] != df.R1[i+1]) :
+#        activitiesPerDay.append(df.R1[i+1])
+#for i in unique:
+#    count = 0
+#    for t in activitiesPerDay :
+#        if(i==t) :
+#            count = count + 1
+#    d.update({i:count})
+##keys are the activities ID
+##values are the number of occurences 
+#ax.scatter(list(d.keys()),list(d.values()))
+#    
+#
+#ndf = pd.DataFrame()
+#ndf['activities'] = list(d.keys())
+#ndf['occurences'] = list(d.values())
+#kmeans = KMeans(n_clusters = 2, random_state=0)
+#kmeans.fit_transform(ndf)
+#centroids = kmeans.cluster_centers_
+#ax.scatter(centroids[:,0], centroids[:,1], marker='x', c='red', alpha=0.5, linewidths=3, s=169)
+#plt.show()
 
-indexes = []
-r1_activities = []
+# Result
+# Two clusters 
+# 1: number of occurrences from 0 to 20
+# 2: number of occurrences from 30 to 70
 
-ndf = pd.DataFrame(columns=columnsName)
+##2 
+## Grouping activities by time required to complete activity
+## Data : 30 days of ARAS data
+df = pd.read_csv('House A/ARAS_Datasets.txt', sep=" ", header=None)
+df.columns = columnsName
+# select only 10 days because it takes too long to load 30 days of data
+df = df.loc[df.index < 864001]
+duration = []
 
-frames = []
-
-for x in range(1, 31):
-    df = pd.read_csv('House A/DAY_'+ str(x) +'.txt', sep=" ", header=None)
-    df.columns = columnsName
-    df = df.loc[(df.index >= 32400) & (df.index < 36000)]
-    frames.append(df)
+activitiesId = [1]
+activitiesPerDay = []
+activitiesPerDay.append(df.R1[0])
+unique = []
+unique = df.R1.unique()
+d = {}
+for activity in activitiesId:
+    ndf = df.loc[df.R1==activity]
+    ndf = ndf.reset_index(drop=True)
+    totalTime = round(((len(ndf)-1) /3600.0),2)
+    for i in range (0, len(df)-1):
+        if(df.R1[i] != df.R1[i+1]) :
+            activitiesPerDay.append(df.R1[i+1]) 
+    count = 0
+    for t in activitiesPerDay :
+        if(t == activity) :
+            count = count + 1
+    avg = round (totalTime / count,2)
+    d.update({activity:avg})
     
-ndf = pd.concat(frames)
+print d
+                
 
-ndf['time'] = ndf.index
-
-ax.scatter(ndf.index, ndf.R1, marker='.', alpha=0.3)
-kmeans = KMeans(n_clusters = 6)
-ndf = ndf[['time','R1']]
-kmeans.fit(ndf)
-labels = kmeans.predict(ndf)
-centroids = kmeans.cluster_centers_
-ax.scatter(centroids[:,0], centroids[:,1], marker='x', c='red', alpha=0.5, linewidths=3, s=169)
-plt.show()
-
+#3 
+# Grouping sensors
+# Data : 30 days of ARAS data
 
 
